3aed5f14ab0afca2fef95bc0330516b97ad11b95
Cryo79 <johannes2503@gmail.com>
2023-01-12 14:34:28 +0000

    Working on rating function for api

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MoviesSerializer, RatingSerializer
 
@@ -6,6 +9,32 @@ from .serializers import MoviesSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MoviesSerializer
+
+    @action(detail=True, methods=["POST"])
+    def rate_movie(self, request, pk=None):
+        if "stars" in request.data:
+
+            movie = Movie.objects.get(id=pk)
+            stars = request.data["stars"]
+            user = User.objects.get(id=1)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serilizer = RatingSerializer(rating, many=False)
+                response = {"messasge": "Rating updated", "result": serilizer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serilizer = RatingSerializer(rating, many=False)
+                response = {"messasge": "Rating created", "result": serilizer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {"message": "You need to provide stars"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
