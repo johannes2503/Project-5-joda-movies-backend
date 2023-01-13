6d80d643ac386f42fa08d5d0904c0767d9c8fb18
Cryo79 <johannes2503@gmail.com>
2023-01-13 20:46:04 +0000

    Added authentication to project

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MoviesSerializer, RatingSerializer
@@ -9,6 +10,7 @@ from .serializers import MoviesSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MoviesSerializer
+    authentication_classes = (TokenAuthentication, )
 
     @action(detail=True, methods=["POST"])
     def rate_movie(self, request, pk=None):
@@ -16,7 +18,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 
             movie = Movie.objects.get(id=pk)
             stars = request.data["stars"]
-            user = User.objects.get(id=1)
+            user = request.user
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -40,3 +42,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
